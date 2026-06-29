821c0418545e7b02ddbf5302c74fdc08910392f8
Nandus86 <nandusempreendimentos@gmail.com>
2026-06-29 19:09:23 -0300

    chore: initial fixes for 0.2.0 branch (env, docker, ngFor)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api/v1',
+    apiUrl: 'http://localhost:8010/api/v1',
     appName: 'Exousía School',
     appSubtitle: 'by Dexos',
 };
